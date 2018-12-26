772dfd1097aabbe6b2f5bee07f7b9d5537ad9fc3
George Holdridge <george@holdridgelabs.com>
2018-12-26 14:48:25 -0500

    Basic setup of MVC routing and a home controller/view

---
+++ BundleSample/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Web;
+using System.Web.Routing;
+
+namespace BundleSample
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Debug.WriteLine("***************************** Application Start ********************************");
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
